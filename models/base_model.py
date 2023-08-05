2cc374d2cb6b79b60c872b06ab96d45e999ae648
othmane <othma125@hotmail.com>
2023-08-05 12:30:38 +0100

    base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
 """ Module that contains class Base """
+from datetime import datetime
 from uuid import uuid4
 
 
 class BaseModel:
     """ Base class """
 
-    def __init__(self, identifier=None):
+    def __init__(self):
         """ constructor """
-        self.id = str(uuid4())
-        # self.created_at = now
+        self.id: str = str(uuid4())
+        self.created_at: datetime = datetime.now()
+        self.updated_at = self.created_at
+
+    def __str__(self):
+        return f"[{self.__class__.__name__} ({self.id}) {self.__dict__}]"
