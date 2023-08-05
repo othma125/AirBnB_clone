1a9b430f4444dbc6ff91ea352c3fa0901b5caf12
othmane <othma125@hotmail.com>
2023-08-05 12:39:30 +0100

    base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """ Module that contains class Base """
+import sys
+import time
 from datetime import datetime
 from uuid import uuid4
 
@@ -14,4 +16,16 @@ class BaseModel:
         self.updated_at = self.created_at
 
     def __str__(self):
+        """ to string """
         return f"[{self.__class__.__name__} ({self.id}) {self.__dict__}]"
+
+    def save(self):
+        self.updated_at = datetime.now()
+
+model = BaseModel()
+model.name = 'first name'
+model.my_number = 98
+print(model)
+time.sleep(2)
+model.save()
+print(model)
