83702ef642c7d35195639cc3f30089d8ac24ece4
othmane <othma125@hotmail.com>
2023-08-05 12:55:19 +0100

    base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """ Module that contains class Base """
-import sys
-import time
+
 from datetime import datetime
 from uuid import uuid4
 
@@ -13,19 +12,30 @@ class BaseModel:
         """ constructor """
         self.id: str = str(uuid4())
         self.created_at: datetime = datetime.now()
-        self.updated_at = self.created_at
+        self.updated_at: datetime = self.created_at
 
     def __str__(self):
         """ to string """
         return f"[{self.__class__.__name__} ({self.id}) {self.__dict__}]"
 
     def save(self):
+        """ update """
         self.updated_at = datetime.now()
 
+    def to_dict(self):
+        """ return dic """
+        dict = self.__dict__.copy()
+        dict['__class__']: str = self.__class__.__name__
+        dict['created_at']: str = str(self.created_at.isoformat())
+        dict['updated_at']: str = str(self.updated_at.isoformat())
+        return dict
+
 model = BaseModel()
-model.name = 'first name'
+model.name:str = 'first name'
 model.my_number = 98
 print(model)
-time.sleep(2)
 model.save()
-print(model)
+tojson = model.to_dict()
+print(tojson)
+for key in tojson.keys():
+    print(f'{key} {type(tojson[key])}')
