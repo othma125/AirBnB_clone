0988b81556a2368917aeaee1398068e555919b75
bensbahou <younesbensbahou@gmail.com>
2023-08-06 22:04:30 +0100

    add check if json file exist

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,6 +2,7 @@
 """ Module that contains class FileStorage """
 from models import BaseModel
 import json
+import os.path
 
 
 class FileStorage:
@@ -13,8 +14,12 @@ class FileStorage:
 
     def reload(self) -> None:
         print("FileStorage.reload()>>>>>>>")
-        with open(self.__file_path, "r") as f:
-            self.__objects = json.load(f)
+
+        if os.path.isfile(self.__file_path):
+            with open(self.__file_path, "r") as f:
+                self.__objects = json.load(f)
+        else:
+            print(f"File {self.__file_path} does not exist.")
 
     def save(self) -> None:
         print("FileStorage.save()>>>>>>>")
