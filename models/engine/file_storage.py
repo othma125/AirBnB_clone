8e61dccb101b4ec600a4a44c2ba47d29042ee908
othmane <othma125@hotmail.com>
2023-08-09 13:09:27 +0100

    console.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -14,26 +14,20 @@ class FileStorage:
 
     def reload(self) -> None:
         """reload method"""
-        # print("FileStorage.reload()>>>>>>>")
         if os.path.isfile(self.__file_path):
             with open(self.__file_path, "r") as f:
                 self.__objects = load(f)
-        # else:
-        #     print(f"File {self.__file_path} does not exist.")
 
     def save(self) -> None:
         """save"""
-        # print("FileStorage.save()>>>>>>>")
         with open(self.__file_path, "w") as f:
             dump(self.__objects, f)
 
     def new(self, obj) -> None:
         """new element is added to dic"""
-        # print("FileStorage.new()>>>>>>>")
         key: str = obj.__class__.__name__ + "." + obj.id
         self.__objects.update({key: obj.to_dict()})
 
     def all(self) -> dict:
         """return data dict"""
-        # print("FileStorage.all()>>>>>>>")
         return self.__objects
